8b6c82455819201669ab9fc5498ad8a39714a283
Lise Gauthier <lise@MacBook-Air-de-Lise.local>
2021-12-19 16:03:27 +0100

    questions supplémentaires agenda + tests

--- src/main/java/agenda/Agenda.java
+++ src/main/java/agenda/Agenda.java
@@ -36,5 +36,36 @@ public class Agenda {
         return res;
     }
 
+    /**
+     * Trouver les événements de l'agenda en fonction de leur titre
+     * @param title le titre à rechercher
+     * @return les événements qui ont le même titre
+     */
+    public List<Event> findByTitle(String title) {
+        List<Event> memeTitre = new ArrayList<>();
+        for (Event e : lesEvents){
+            if (e.getTitle().toLowerCase().equals(title)){
+                memeTitre.add(e);
+            }
+        }   
+        return memeTitre;    
+    }
+    
+    /**
+     * Déterminer s’il y a de la place dans l'agenda pour un événement
+     * @param e L'événement à tester (on se limitera aux événements simples)
+     * @return vrai s’il y a de la place dans l'agenda pour cet événement
+     */
+    public boolean isFreeFor(Event e) {
+        for (Event myEvent : lesEvents){
+            if (e.getStart().equals(myEvent.getStart()) // les 2 events commencent en même temps
+                || e.getStart().isAfter(myEvent.getStart()) && e.getStart().isBefore(myEvent.getStart().plus(myEvent.getDuration())) // l'event que l'on teste débute pendant un autre event (avant la fin d'un autre event)
+                || e.getStart().isBefore(myEvent.getStart()) && e.getStart().plus(e.getDuration()).isAfter(myEvent.getStart()) // l'event que l'on teste termine après le début d'un autre event
+                ){
+                    return false;
+                }
+        }
+        return true;       
+    }
    
 }
--- src/test/java/agenda/AgendaTest.java
+++ src/test/java/agenda/AgendaTest.java
@@ -21,12 +21,27 @@ public class AgendaTest {
     // November 1st, 2020, 22:30
     LocalDateTime nov_1__2020_22_30 = LocalDateTime.of(2020, 11, 1, 22, 30);
 
+    //Nouvelles dates
+    LocalDateTime nov_1__2020_21_30 = LocalDateTime.of(2020, 11, 1, 21, 30);
+    LocalDateTime nov_1__2020_23_30 = LocalDateTime.of(2020, 11, 1, 23, 30);
+    LocalDateTime dec_1_2021_13_00 = LocalDateTime.of(2021, 12, 1, 13, 00);
+    LocalDateTime dec_15_2021_13_00 = LocalDateTime.of(2021, 12, 15, 13, 00);
+    LocalDateTime dec_2_2021_14_00 = LocalDateTime.of(2021, 12, 2, 14, 00);
+
     // 120 minutes
     Duration min_120 = Duration.ofMinutes(120);
+
+    // Autres durées
+    Duration min_60 = Duration.ofMinutes(60);
 
     // A simple event
     // November 1st, 2020, 22:30, 120 minutes
     Event simple = new Event("Simple event", nov_1__2020_22_30, min_120);
+
+    //New events
+    Event piscine1 = new Event("Piscine", dec_1_2021_13_00, min_120);
+    Event piscine2 = new Event("Piscine", dec_15_2021_13_00, min_120);
+    Event patinoire = new Event("Patinoire", dec_1_2021_13_00, min_120);
 
     // A Weekly Repetitive event ending at a given date
     RepetitiveEvent fixedTermination = new FixedTerminationEvent("Fixed termination weekly", nov_1__2020_22_30, min_120, ChronoUnit.WEEKS, jan_5_2021);
@@ -53,5 +68,30 @@ public class AgendaTest {
         assertTrue(agenda.eventsInDay(nov_1_2020).contains(neverEnding));
     }
 
+    @Test
+    public void testFindByTitle() {
+        agenda.addEvent(piscine1);
+        agenda.addEvent(piscine2);
+        agenda.addEvent(patinoire);
 
+        assertEquals(2, agenda.findByTitle("piscine").size(), "Il y a 2 événements de ce nom");
+        assertEquals(0, agenda.findByTitle("luge").size(), "Il y a 0 événement de ce nom");
+    }
+
+    @Test
+    public void testAgendaLibre() {
+        //création d'un event qui doit débuter en même temps que le simpleEvent déjà programmé
+        Event simpleMemeTemps = new Event("Simple event", nov_1__2020_22_30, min_60);
+
+        //création d'un event qui doit débuter avant la fin du simpleEvent déjà programmé
+        Event simpleDeb = new Event("Simple event", nov_1__2020_23_30, min_120);
+        
+        //création d'un event qui doit terminer après le début du simpleEvent déjà programmé
+        Event simpleFin = new Event("Simple event", nov_1__2020_21_30, min_120);
+
+        assertTrue(agenda.isFreeFor(piscine1), "Les 2 évènements ne se chevauchent pas");
+        assertFalse(agenda.isFreeFor(simpleMemeTemps), "Un evenement est déjà programmé au même moment");
+        assertFalse(agenda.isFreeFor(simpleDeb), "Un evenement est déjà programmé et n'est pas encore terminé");
+        assertFalse(agenda.isFreeFor(simpleFin), "Un autre evenement est déjà programmé avant que celui-ci ne soit terminé");
+    }
 }
